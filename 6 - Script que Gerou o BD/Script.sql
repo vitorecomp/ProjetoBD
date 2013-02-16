34b7f26f64a0ca1d1bfb4152c749a977f03e8701
vitorecomp <vitor.ecomp@gmail.com>
2013-02-16 13:48:14 -0200

    Pastas / Script

---
+++ 6 - Script que Gerou o BD/Script.sql
@@ -0,0 +1,82 @@
+DROP TABLE IF EXISTS Estadio;
+DROP TABLE IF EXISTS Paises;
+DROP TABLE IF EXISTS Bolao;
+DROP TABLE IF EXISTS Gols;
+DROP TABLE IF EXISTS Pessoas;
+DROP TABLE IF EXISTS Balao_has_Pessoas;
+DROP TABLE IF EXISTS Integrantes;
+DROP TABLE IF EXISTS Pessoas_has_Torcida;
+DROP TABLE IF EXISTS Cartao;
+DROP TABLE IF EXISTS Integrantes_has_Cartao;
+DROP TABLE IF EXISTS Torcida;
+DROP TABLE IF EXISTS Copa;
+DROP TABLE IF EXISTS Integrantes_has_Gols;
+DROP TABLE IF EXISTS Torcida_has_Copa;
+DROP TABLE IF EXISTS Equipe;
+DROP TABLE IF EXISTS Jogos;
+DROP TABLE IF EXISTS Equipe_has_Integrantes;
+DROP TABLE IF EXISTS Jogos_has_Bolao;
+
+CREATE TABLE Estadio
+	(
+		idEstadio INTEGER USIGNED PRIMARY KEY AUTO_INCREMENT,
+		Cidade varchar(65) NOT NULL,
+		Capacidade DOUBLE UNSIGNED NOT NULL,
+		Nome varchar(45) NOT NULL
+	);
+
+CREATE TABLE Estadio
+	(
+
+	);
+
+CREATE TABLE Estadio
+	(
+
+	);
+
+CREATE TABLE Estadio
+	(
+
+	);
+
+CREATE TABLE Estadio
+	(
+
+	);
+
+CREATE TABLE Estadio
+	(
+
+	);
+
+CREATE TABLE Estadio
+	(
+
+	);
+
+CREATE TABLE Estadio
+	(
+
+	);
+
+CREATE TABLE Estadio
+	(
+
+	);
+
+CREATE TABLE Estadio
+	(
+
+	);
+
+CREATE TABLE Estadio
+	(
+
+	);
+
+CREATE TABLE Estadio
+	(
+
+	);
+
