9701b4b39c9f03e089a231cdf50f2ea491b5d334
vitorecomp <vitor.ecomp@gmail.com>
2013-02-17 16:14:31 -0300

    fim da entidades, ficou feio vo passar alguma cois para tipo basico

--- ProjetoNetBeans/src/trabalho/Entidades/Pessoa.java
+++ ProjetoNetBeans/src/trabalho/Entidades/Pessoa.java
@@ -7,7 +7,44 @@ package Trabalho.Entidades;
 import trabalho.TiposBasicos.Data;
 
 public class Pessoa {
-    int id;
-    String nome;
-    Data datanasc;
+
+    private int id;
+    private String nome;
+    private Data datanasc;
+
+    int getId() {
+        return this.id;
+    }
+
+    String getString() {
+        return this.nome;
+    }
+
+    Data getData() {
+        return this.datanasc;
+    }
+    
+    void setId(int id) throws IllegalArgumentException {
+        if (id >= 0) {
+            this.id = id;
+        } else {
+            throw new IllegalArgumentException("id invalido");
+        }
+    }
+    
+    void setNome(String nome) throws IllegalArgumentException {
+        if (nome.length() <= 45) {
+            this.nome = nome;
+        } else {
+            throw new IllegalArgumentException("nome invalido");
+        }
+    }
+    
+    void setData(Data dataNasc) throws IllegalArgumentException {
+        if (dataNasc != null) {
+            this.datanasc = dataNasc;
+        } else {
+            throw new IllegalArgumentException("data vazia");
+        }
+    }
 }
--- ProjetoNetBeans/src/trabalho/Entidades/Torcida.java
+++ ProjetoNetBeans/src/trabalho/Entidades/Torcida.java
@@ -7,8 +7,55 @@ package Trabalho.Entidades;
 import trabalho.TiposBasicos.Foto;
 
 public class Torcida {
-    int id;
-    int Pais;
-    Foto foto;
-    int Presidente;
+
+    private int id;
+    private int pais;
+    private Foto foto;
+    private int presidente;
+
+    int getId() {
+        return this.id;
+    }
+
+    int getPais() {
+        return this.pais;
+    }
+
+    Foto getFoto() {
+        return this.foto;
+    }
+    
+    int getPresidente()
+    {
+        return this.presidente;
+    }
+    
+    void setId(int id) throws IllegalArgumentException {
+        if (id >= 0) {
+            this.id = id;
+        } else {
+            throw new IllegalArgumentException("id invalido");
+        }
+    }
+    void setPais(int pais) throws IllegalArgumentException {
+        if (pais >= 0) {
+            this.pais = pais;
+        } else {
+            throw new IllegalArgumentException("pais invalido");
+        }
+    }
+    void setFoto(Foto foto) throws IllegalArgumentException {
+        if (foto != null) {
+            this.foto = foto;
+        } else {
+            throw new IllegalArgumentException("foto vazia");
+        }
+    }
+    void setPresidente(int presidente) throws IllegalArgumentException {
+        if (presidente >= 0) {
+            this.presidente = presidente;
+        } else {
+            throw new IllegalArgumentException("presidente invalido");
+        }
+    }
 }
