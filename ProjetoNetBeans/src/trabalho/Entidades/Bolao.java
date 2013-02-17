af8dfa0dab993307f9bfa7a56cca588f68719337
vitorecomp <vitor.ecomp@gmail.com>
2013-02-17 14:23:13 -0300

    bolao cartao e copa

--- ProjetoNetBeans/src/trabalho/Entidades/Bolao.java
+++ ProjetoNetBeans/src/trabalho/Entidades/Bolao.java
@@ -3,10 +3,49 @@
  * and open the template in the editor.
  */
 package Trabalho.Entidades;
+
 import trabalho.TiposBasicos.*;
 
 public class Bolao {
-    int id;
-    String Aposta;
-    Data data;
+
+    private int id;
+    private String aposta;
+    private Data data;
+
+    int getId() {
+        return this.id;
+    }
+
+    String getAposta() {
+        return this.aposta;
+    }
+
+    Data getData() {
+        return this.data;
+    }
+
+    void setID(int id) throws IllegalArgumentException {
+        if (id > 0) {
+            this.id = id;
+        } else {
+            throw new IllegalArgumentException("idInvalido");
+        }
+    }
+
+    void setAposta(String aposta) throws IllegalArgumentException {
+        if (aposta.length() <= 45) {
+            this.aposta = aposta;
+        } else {
+            throw new IllegalArgumentException("apostaImvalida");
+        }
+
+    }
+
+    void setData(Data data) throws IllegalArgumentException {
+        if (data != null) {
+            this.data = data;
+        } else {
+            throw new IllegalArgumentException("dataInvalida");
+        }
+    }
 }
--- ProjetoNetBeans/src/trabalho/Entidades/Copa.java
+++ ProjetoNetBeans/src/trabalho/Entidades/Copa.java
@@ -3,12 +3,61 @@
  * and open the template in the editor.
  */
 package Trabalho.Entidades;
+
 import trabalho.TiposBasicos.Data;
 
 public class Copa {
-    int ano;
-    int pais;
-    Data inicio;
-    Data fim;
-    
+
+    private int ano;
+    private int pais;
+    private Data inicio;
+    private Data fim;
+
+    int getAno() {
+        return this.ano;
+    }
+
+    int getPais() {
+        return this.pais;
+    }
+
+    Data getInicio() {
+        return this.inicio;
+    }
+
+    Data getFim() {
+        return this.fim;
+    }
+
+    void setAno(int ano) throws IllegalArgumentException {
+        if (ano > 1800) {
+            this.ano = ano;
+        } else {
+            throw new IllegalArgumentException("ano invalido");
+        }
+    }
+
+    void setPais(int pais) throws IllegalArgumentException {
+        if (pais > 0) {
+            this.pais = pais;
+        } else {
+            throw new IllegalArgumentException("pais invalido");
+        }
+    }
+
+    void setIncio(Data inicio) throws IllegalArgumentException {
+        if (inicio != null) {
+            this.inicio = inicio;
+        } else {
+            throw new IllegalArgumentException("data de inicio vazia");
+        }
+    }
+
+    void setFim(Data fim) throws IllegalArgumentException {
+        if (fim != null) {
+            this.fim = fim;
+        } else {
+            throw new IllegalArgumentException("data de termino vazia");
+        }
+    }
 }
