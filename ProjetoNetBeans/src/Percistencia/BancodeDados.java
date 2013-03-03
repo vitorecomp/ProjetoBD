bed6082b63270bab6a861b0a0017fdf15b7d5f11
Igor <igormiranda5@gmail.com>
2013-03-03 17:53:44 -0300

    BancodeDados.java

--- ProjetoNetBeans/src/Percistencia/BancodeDados.java
+++ ProjetoNetBeans/src/Percistencia/BancodeDados.java
@@ -8,6 +8,33 @@ package Percistencia;
  *
  * @author Vitor
  */
+import java.sql.*;
+
 public class BancodeDados {
-    
+    // Access
+
+    private static String DRIVER_BD = "sun.jdbc.odbc.JdbcOdbcDriver";
+    private static String URL_BD = "jdbc:mysql://localhost";
+    private static String usuario = "";
+    private static String senha = "";
+    private static Connection con;
+
+    public BancodeDados() {
+        try {
+            Class.forName(DRIVER_BD);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Problemas ao carregar o driver");
+        }
+    }
+
+    public static Connection getConexao() {
+        try {
+            if ((con == null) || (con.isClosed())) {
+                con = DriverManager.getConnection(URL_BD, usuario, senha);
+            }
+        } catch (SQLException e) {
+            System.out.println("Problemas ao abrir a conexao com o BD");
+        }
+        return con;
+    }
 }
